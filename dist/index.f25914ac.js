47d34ac7edf38fd8556e1af726de81a275da65f5
wangdisheng <wangdishengyx@163.com>
2022-04-23 14:20:05 +0800

    代码优化，代码播放器封装

--- dist/index.f25914ac.js
+++ dist/index.f25914ac.js
@@ -458,66 +458,74 @@ function hmrAcceptRun(bundle, id) {
 var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
 var _css = require("./css");
 var _cssDefault = parcelHelpers.interopDefault(_css);
-let styleContent = document.getElementById("styleContent");
-let styleText = document.querySelector("#styleText");
-// const btnPause = document.querySelector("#btnPause");
-const btnPause = document.getElementById("btnPause");
-// const btnSlow = document.querySelector("#btnSlow");
-const btnSlow = document.getElementById("btnSlow");
-// const btnNormal = document.querySelector("#btnNormal");
-// const btnFast = document.querySelector("#btnFast");
-const btnFast = document.getElementById("btnFast");
-const pause = ()=>{
-    btnPause.innerHTML = "播放";
-    flag = 1; // 暂停
-    clock && window.clearTimeout(clock); // 取消定时器
-};
-const play = ()=>{
-    btnPause.innerHTML = "暂停";
-    flag = 0;
-    step();
-};
-const pauseAndPlay = ()=>(flag === 0 ? pause : play).call(null)
-;
-const slow = ()=>speed = speed >= 10 ? speed + 10 : speed + 1
-;
-const fast = ()=>speed = speed > 0 ? speed > 10 ? speed - 10 : speed - 1 : 0
-;
-btnPause.onclick = pauseAndPlay;
-btnSlow.onclick = slow;
-btnFast.onclick = fast;
-let srtFinal = "";
-let n = 0;
-let flag = 0;
-let speed = 0;
-let clock;
-function step() {
-    srtFinal = srtFinal + trans(_cssDefault.default[n]);
-    styleText.innerHTML = srtFinal;
-    styleContent.innerHTML = _cssDefault.default.substring(0, n);
-    window.scrollTo(0, 99999); /*这是window的滚动条*/ 
-    styleText.scroll(0, 99999); /*这是div的滚动条*/ 
-    n += 1;
-    if (n >= _cssDefault.default.length) {
-        srtFinal = "";
-        n = 0;
-        pause();
-        return;
+const player = {
+    n: 0,
+    strFinal: "",
+    flag: 0,
+    speed: 0,
+    clock: undefined,
+    init: ()=>{
+        player.bindEvents();
+        player.step();
+    },
+    ui: {
+        styleContent: document.getElementById("styleContent"),
+        styleText: document.querySelector("#styleText"),
+        btnPause: document.getElementById("btnPause")
+    },
+    events: {
+        "#btnPause": "pauseAndPlay",
+        "#btnSlow": "slow",
+        "#btnFast": "fast"
+    },
+    bindEvents: ()=>{
+        for(let key in player.events)if (player.events.hasOwnProperty(key)) // @ts-ignore
+        document.querySelector(key).onclick = player[player.events[key]];
+    },
+    pause: ()=>{
+        player.ui.btnPause.innerHTML = "播放";
+        player.flag = 1; // 暂停
+        player.clock && window.clearTimeout(player.clock); // 取消定时器
+    },
+    play: ()=>{
+        player.ui.btnPause.innerHTML = "暂停";
+        player.flag = 0;
+        player.step();
+    },
+    pauseAndPlay: ()=>(player.flag === 0 ? player.pause : player.play).call(null)
+    ,
+    slow: ()=>player.speed = player.speed >= 10 ? player.speed + 10 : player.speed + 1
+    ,
+    fast: ()=>player.speed = player.speed > 0 ? player.speed > 10 ? player.speed - 10 : player.speed - 1 : 0
+    ,
+    step: ()=>{
+        player.strFinal = player.strFinal + player.trans(_cssDefault.default[player.n]);
+        player.ui.styleText.innerHTML = player.strFinal;
+        player.ui.styleContent.innerHTML = _cssDefault.default.substring(0, player.n);
+        window.scrollTo(0, 99999); /*这是window的滚动条*/ 
+        player.ui.styleText.scroll(0, 99999); /*这是div的滚动条*/ 
+        player.n += 1;
+        if (player.n >= _cssDefault.default.length) {
+            player.strFinal = "";
+            player.n = 0;
+            player.pause();
+            return;
+        }
+        if (player.flag === 1) return;
+        player.clock = setTimeout(player.step, player.speed * 10);
+    },
+    trans: (ch)=>{
+        switch(ch){
+            case '\n':
+                return '<br>';
+            case ' ':
+                return '&nbsp;';
+            default:
+                return ch;
+        }
     }
-    if (flag === 1) return;
-    clock = setTimeout(step, speed * 10);
-}
-function trans(ch) {
-    switch(ch){
-        case '\n':
-            return '<br>';
-        case ' ':
-            return '&nbsp;';
-        default:
-            return ch;
-    }
-}
-step();
+};
+player.init();
 
 },{"./css":"4hgyY","@parcel/transformer-js/src/esmodule-helpers.js":"hrokx"}],"4hgyY":[function(require,module,exports) {
 var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
