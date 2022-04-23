cbf103522ad0d1d99ca012b2189067c68b3fc45c
wangdisheng <wangdishengyx@163.com>
2022-04-23 11:52:39 +0800

    代码优化，函数形式优化

--- src/main.ts
+++ src/main.ts
@@ -7,22 +7,27 @@ const btnPause = document.getElementById("btnPause");
 // const btnSlow = document.querySelector("#btnSlow");
 const btnSlow = document.getElementById("btnSlow");
 // const btnNormal = document.querySelector("#btnNormal");
-const btnFast = document.querySelector("#btnFast");
-let pause = () => {
+// const btnFast = document.querySelector("#btnFast");
+const btnFast = document.getElementById("btnFast");
+
+const pause = () => {
     btnPause.innerHTML = "播放";
     flag = 1; // 暂停
     clock && window.clearTimeout(clock); // 取消定时器
 }
-let play = () => {
+const play = () => {
     btnPause.innerHTML = "暂停";
     flag = 0;
     step();
 }
-btnPause.onclick = () =>(flag === 0 ? pause : play).call(null);
-btnSlow.onclick = () => speed = speed >= 10 ? speed + 10 : speed + 1;
-btnFast.addEventListener('click',(e)=>{
-    speed = speed > 0 ? (speed > 10 ? speed - 10 : speed - 1) : 0;
-})
+
+const pauseAndPlay = () =>(flag === 0 ? pause : play).call(null);
+const slow = () => speed = speed >= 10 ? speed + 10 : speed + 1;
+const fast = () => speed = speed > 0 ? (speed > 10 ? speed - 10 : speed - 1) : 0;
+
+btnPause.onclick = pauseAndPlay;
+btnSlow.onclick = slow;
+btnFast.onclick = fast;
 
 let srtFinal = "";
 let n= 0;
